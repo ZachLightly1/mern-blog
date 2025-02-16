d95a7be2ac7014cb00346a2988828d861fd6eeed
Zach <zachlightly@gmail.com>
2025-02-15 19:46:21 -0600

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    console.log(req.body);
 
     if (
         !username ||
@@ -12,12 +12,9 @@ export const signup = async (req, res, next) => {
         !password ||
         username === "" ||
         email === "" ||
-        password === "" ||
-        username == undefined ||
-        email == undefined ||
-        password == undefined
+        password === ""
     ) {
-        next(errorHandler(400, "All fields are required"));
+        return next(errorHandler(400, "All fields are required"));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -30,6 +27,43 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.json({ message: "Signup successful" });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === "" || password === "") {
+        next(errorHandler(400, "All fields are required"));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, "Invalid credentials")); // no user
+        }
+
+        const validPassword = bcryptjs.compareSync(
+            password,
+            validUser.password
+        );
+        if (!validPassword) {
+            return next(errorHandler(400, "Invalid credentials")); // wrong password
+        }
+
+        const { password: pass, ...rest } = validUser._doc;
+
+        const token = jwt.sign(
+            {
+                id: validUser._id,
+            },
+            process.env.JWT_SECRET
+        );
+        res.status(200)
+            .cookie("access_token", token, { httpOnly: true })
+            .json(rest);
     } catch (error) {
         next(error);
     }
