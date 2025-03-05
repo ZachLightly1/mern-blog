30dc7d401bc6e372f995e23b1ae601320b2a6b81
Zach <zachlightly@gmail.com>
2025-03-04 22:36:05 -0600

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import imageRoutes from "./routes/image.route.js";
+import postRoutes from "./routes/post.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -32,6 +33,7 @@ app.listen(3000, () => {
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/image", imageRoutes);
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
